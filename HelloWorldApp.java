539ed227a713c689fbb80b3ca2f07f65ac093b04
Hoang Son <wingadium1@gmail.com>
2019-07-31 15:30:32 +0700

    init

---
+++ HelloWorldApp.java
@@ -0,0 +1,7 @@
+class HelloWorldApp {
+    public static void main(String[] args) {
+        System.out.println("Hello World master and issues1!"); // Display the string.
+        System.out.println("Hello World master and issues2!"); // Display the string.
+    }
+}
+
